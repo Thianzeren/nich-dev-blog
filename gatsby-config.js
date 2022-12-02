4ba303e583c823f9bac658abc42664acb153d832
Thianzeren <nicholasthian_1995@hotmail.com>
2022-12-02 08:44:12 +0800

    added plugins for images and tested images

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
     title: `Nich Dev Blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-netlify"]
+  plugins: [
+    "gatsby-plugin-netlify",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    }
+  ]
 };
